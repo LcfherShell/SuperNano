16521e8a6ca7f3b7af2a375e2fe7f2cd26190ab8
LcferShell <71859305+LcfherShell@users.noreply.github.com>
2024-09-01 23:53:04 +0700

    setup.py
    Signed-off-by: LcferShell <71859305+LcfherShell@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ setup(
     version="2.2.1",
     author="LcfherShell",
     author_email="alfiandecker2@gmail.com",
-    description="A console-based text editor built with Python and curses.",
+    description="A console-based text editor.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/LcfherShell/SuperNano",
-    packages=find_packages(),  # This automatically finds both 'supernano' and 'utils'
+    py_modules=["main", "install"],   # This automatically finds both 'supernano' and 'utils'
     include_package_data=True,
     install_requires=[
       # Add other dependencies here
     ],
     entry_points={
         "console_scripts": [
-            "supernano=supernano.__main__:main",  # Entry point untuk __main__.py
-            "isupernano=supernano.install:install_script",  # Entry point untuk install.py
+            "supernano=main:main",  # Entry point untuk __main__.py
+            "isupernano=install:install_script",  # Entry point untuk install.py
         ],
     },
     classifiers=[
